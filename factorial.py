4c16c0be40323f22f9c7205790b4d9c912f598d2
Subramanya Malleshi <SMalleshi@jmawireless.com>
2025-09-12 08:33:48 +0530

    Module 3 assignment

---
+++ factorial.py
@@ -0,0 +1,10 @@
+n = int(input('Enter a number: '))
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+print(f"The factorial of {n} is {factorial(n)}")
